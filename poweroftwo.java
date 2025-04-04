c8a9c6e63d5713bbfc186592271b3dcd45343172
Ansh Sharma <ansh88179@gmail.com>
2025-04-04 12:24:37 +0530

    Update poweroftwo.java

--- poweroftwo.java
+++ poweroftwo.java
@@ -4,7 +4,7 @@ public static void main(String[] args){
 Scanner Sc= new Scanner(System.in);
 System.out.println("Enter a number:");
 int num=Sc.nextInt();
-boolean ispoweroftwo=(num>0)&&(num&(num-1))==0;
+boolean ispoweroftwo=(num>0)&&((num&(num-1))==0);
 System.out.println("ispoweroftwo");
 }
 }
