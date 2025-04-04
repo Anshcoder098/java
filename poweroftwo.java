4d174ac8e45241d1368466703224a8ba115b0577
Ansh Sharma <ansh88179@gmail.com>
2025-04-04 11:46:12 +0530

    Add files via upload

---
+++ poweroftwo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class poweroftwo{
+public static void main(String[] args){
+Scanner Sc= new Scanner(System.in);
+System.out.println("Enter a number:");
+int num=Sc.nextInt();
+boolean ispoweroftwo=(num>0)&&(num&(num-1))==0;
+System.out.println("ispoweroftwo");
+}
+}
